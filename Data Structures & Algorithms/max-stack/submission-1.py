4a387465acc1fb174fcd2932a30af5d6ad0f1100
Jamshid <60542950+Jamshidbekdev@users.noreply.github.com>
2026-08-12 16:06:24 +0500

    Add: max-stack - submission-1

---
+++ Data Structures & Algorithms/max-stack/submission-1.py
@@ -0,0 +1,30 @@
+class MaxStack:
+
+    def __init__(self):
+        self.stack = []
+        self.maxAt = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        current_max = x if not self.maxAt else max(x, self.maxAt[-1])
+        self.maxAt.append(current_max)
+
+    def pop(self) -> int:
+        self.maxAt.pop()
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def peekMax(self) -> int:
+        return self.maxAt[-1]
+
+    def popMax(self) -> int:
+        topMax = self.peekMax()
+        buffer = []
+        while self.top() != topMax:
+            buffer.append(self.pop())
+        self.pop()
+        while buffer:
+            self.push(buffer.pop())
+        return topMax
